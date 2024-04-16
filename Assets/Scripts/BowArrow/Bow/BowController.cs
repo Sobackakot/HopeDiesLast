aa676de629cd8a63d4af8c20cf6719799eadf62e
Aibulat <119883177+Sobackakot@users.noreply.github.com>
2024-04-16 19:32:50 +0300

    Bow

---
+++ Assets/Scripts/BowArrow/Bow/BowController.cs
@@ -0,0 +1,43 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowController : MonoBehaviour
+{
+    [SerializeField] private Transform ropeTransform;
+    private Vector3 freeLocalPosition;
+    [SerializeField] private Vector3 tenseLocalPosition; 
+    private float tension; 
+    public List<Vector3> positions = new List<Vector3>();
+
+    private bool isPressed = false;
+    public void Start()
+    {
+        freeLocalPosition = ropeTransform.localPosition; 
+        foreach(var list in positions)
+        {
+
+        }
+    }
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isPressed = true;
+            
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isPressed = false;
+            tension = 0;
+            ropeTransform.localPosition = freeLocalPosition;
+        }
+        if(isPressed && tension <= 1)
+        {
+            tension += Time.deltaTime;
+            tension = Mathf.Clamp01(tension);
+            ropeTransform.localPosition = Vector3.Lerp(freeLocalPosition, tenseLocalPosition, tension); 
+        }   
+    }
+}
+    
